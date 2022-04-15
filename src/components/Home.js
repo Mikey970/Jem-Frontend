341044fd5ea1f69e6c2860b02094c3319ec833a6
Emil Terrero <emil@Emils-MacBook-Pro.local>
2022-04-15 17:28:54 -0400

    Updates to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,25 @@
 //fire
 import './Home.css';
-
 import Row from './Row';
 import Row2 from './Row2';
 import Row3 from './Row3';
-function Home(props) {
-  return (<div className='home-div'>
-    <Row />
-      <Row2 />
-      <Row3 />
-      </div>);
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+function Home() {
+    const [electronics, setElectronics] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/electronics')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }, [])
+
+    return (<div className='home-div'>
+        <Row />
+        <Row2 />
+        <Row3 />
+        </div>);
 }
 
 
