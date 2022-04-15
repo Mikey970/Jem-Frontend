5edc7ca75fd1a97c8cda5065fdcbe88d1b722ed6
Mike Delucia <mikey970@yahoo.com>
2022-04-15 14:40:31 -0400

    minor improvements to home

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,7 +2,7 @@
 import "../row1.css"
 function Row(props) {
   return (
-    <div>
+    <div className="row1-container">
       <div className="row1" id="row1-1"></div>
 
       <div className="row1" id="row1-2"></div>
--- src/components/Row3.js
+++ src/components/Row3.js
@@ -3,7 +3,7 @@ import "../row3.css"
 
 function Row3(props) {
   return (
-    <div>
+    <div className="row3container">
       <div id="row3-1" className="row3"></div>
       <div id="row3-2" className="row3"></div>
     </div>
