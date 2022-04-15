7d843ae14f66f9031cd19768b2451a269d0589a9
Joseph <polanco.joseph1@gmail.com>
2022-04-15 16:14:26 -0400

    link nav routes created

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,23 @@
 import './Nav.css';
 import Dropdown from 'react-bootstrap/Dropdown';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <div className='navbar'>
         <div className='btns-div'>
-        <button className='home-btn'>Home</button>
-        <button className='cart-btn'>Cart</button>
+        <button className='home-btn'><Link to="/">Home</Link></button>
+        <button className='cart-btn'><Link to="/cart">Cart</Link></button>
         <Dropdown>
         <Dropdown.Toggle variant='dark' id='dropdown-shop'>
         Shop
         </Dropdown.Toggle>
         <Dropdown.Menu>
-        <Dropdown.Item href="/#">TVs</Dropdown.Item>
-        <Dropdown.Item href="/#">Consoles</Dropdown.Item>
-        <Dropdown.Item href="/#">Phones</Dropdown.Item>
-        <Dropdown.Item href="/#">PCs/Laptops</Dropdown.Item>
-        <Dropdown.Item href="/#">Headphones</Dropdown.Item>
+        <Dropdown.Item><Link to="/shop">TVs</Link></Dropdown.Item>
+        <Dropdown.Item><Link to="/shop">Consoles</Link></Dropdown.Item>
+        <Dropdown.Item><Link to="/shop">Phones</Link></Dropdown.Item>
+        <Dropdown.Item><Link to="/shop">PCs/Laptops</Link></Dropdown.Item>
+        <Dropdown.Item><Link to="/shop">Headphones</Link></Dropdown.Item>
         </Dropdown.Menu>
         </Dropdown>
         </div>
