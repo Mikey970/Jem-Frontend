ce1c96ea4d2cc971d9476402ad3c1b7ff167bc75
Emil Terrero <emil@Emils-MacBook-Pro.local>
2022-04-15 14:15:38 -0400

    Updating repo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 //fire
+import './Home.css';
 
 import Row from './Row';
 import Row2 from './Row2';
 import Row3 from './Row3';
 function Home(props) {
-  return (<div>
+  return (<div className='home-div'>
     <Row />
       <Row2 />
       <Row3 />
