6df9f68cc26a3b852ee578f172144d0c4b2d2c95
Mike Delucia <mikey970@yahoo.com>
2022-04-15 12:49:30 -0400

    first component worked on

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router} from "react-router-dom";
-
+import Row from './components/Row';
+import Row2 from './components/Row2';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Row />
+      <Row2 />
+      </div>
   );
 }
 
---
+++ src/components/Row2.js
@@ -0,0 +1,16 @@
+//fire
+import "../row2.css"
+
+function Row2(props) {
+  return (
+    <div>
+      <div className="box"><img></img><p>Earbuds</p></div>
+      <div className="box"><img></img><p>TVs</p></div>
+      <div className="box"><img></img><p>Consoles</p></div>
+      <div className="box"><img></img><p>Phones</p></div>
+      <div className="box"><img></img><p>Laptops</p></div>
+    </div>
+  )
+}
+
+export default Row2;
