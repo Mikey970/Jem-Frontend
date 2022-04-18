710e5fb278049de5d6a316d3edb31ee0c8a73e01
Joseph <polanco.joseph1@gmail.com>
2022-04-18 09:51:02 -0400

    updated shop add api calls

--- src/components/ShopCard.js
+++ src/components/ShopCard.js
@@ -1,8 +1,22 @@
 import './ShopCard.css';
+import { useEffect, setState } from 'react';
+import axios from 'axios';
 
-const ShopCard = () => {
+const ShopCard = (props) => {
+  let { electronic } = props
+  useEffect(() => { 
+    
+  }, []);
     return (
-        <div className='card-div'>
+      <div className='card-div'>
+        <div className='shop-card-image-container'>
+
+        </div>
+        <div className='shop-card-title'></div>
+        <div className='shop-card-price'></div>
+        <div className='add-to-cart'>
+          <button>Add to cart</button>
+        </div>
         </div>
     );
 }
